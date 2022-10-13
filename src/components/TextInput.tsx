4fd46c99e40ded1a22aa944d76d880aa1e30f63a
Jorge Lucas <37757146+thepinkwheat@users.noreply.github.com>
2022-10-13 15:26:41 -0300

    Atualizações
    O erro no TextInput.Icon era a falta de um return... é...

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,13 +1,13 @@
-import { Slot } from '@radix-ui/react-slot'
-import { InputHTMLAttributes, ReactNode } from 'react'
+import { InputHTMLAttributes, ReactNode } from 'react';
+import { Slot } from '@radix-ui/react-slot';
 
-export interface TextInputRootProps{
+export interface TextInputRootProps {
   children: ReactNode;
 }
 
-function TextInputRoot(props: TextInputRootProps){
-  return(
-    <div className='flex items-center gap-3 py-4 px-3 rounded bg-gray-800 focus-within:ring-2 ring-cyan300 w-full h-12'>
+function TextInputRoot(props: TextInputRootProps) {
+  return (
+    <div className="flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300">
       {props.children}
     </div>
   )
@@ -15,25 +15,28 @@ function TextInputRoot(props: TextInputRootProps){
 
 TextInputRoot.displayName = 'TextInput.Root'
 
-export interface TextInputIconProps{
-  children: ReactNode;
+export interface TextInputIconProps {
+  children?: ReactNode;
 }
 
-function TextInputIcon(props: TextInputIconProps){
-  <Slot className="m-6 h-6 text-gray-400">
-    {props.children}
-  </Slot>
+function TextInputIcon(props: TextInputIconProps) {
+  return (
+    <Slot className="w-6 h-6 text-gray-400">
+      {props.children}
+    </Slot>
+  )
 }
 
 TextInputIcon.displayName = 'TextInput.Icon'
 
 export interface TextInputInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-function TextInputInput(props:TextInputInputProps){
+function TextInputInput(props: TextInputInputProps) {
   return (
-      <input className="bg-transparent flex-1 text-xs text-gray-100 placeholder:text-gray-400 outline-none" 
+    <input 
+      className="bg-transparent flex-1 text-gray-100 text-xs placeholder:text-gray-400 outline-none"
       {...props}
-      />
+    />
   )
 }
 
