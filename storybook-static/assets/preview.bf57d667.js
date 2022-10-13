1658d4f2c3c5ec962e7a8317ab5cc24a706ffbaa
Jorge Lucas <37757146+thepinkwheat@users.noreply.github.com>
2022-10-13 12:59:18 -0300

    storybook-static

---
+++ storybook-static/assets/preview.bf57d667.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.d4fc5b24.js";import"./iframe.17b2ade8.js";import"./es.map.constructor.27b31bdf.js";import"./es.number.to-fixed.40baf444.js";import"./index.a60e0698.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.bf57d667.js.map
