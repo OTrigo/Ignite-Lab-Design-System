30680d28e6efb6226d018a050386e7ef4169bd5e
Jorge Lucas <37757146+thepinkwheat@users.noreply.github.com>
2022-10-14 15:24:08 -0300

    Automação de testes
    Há um erro na importação do rest do msw, ter que ver isso depois.

---
+++ storybook-static/assets/preview.b5140ae1.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.09d03b5d.js";import"./iframe.bb51801b.js";import"./es.map.constructor.71074591.js";import"./es.number.to-fixed.bc466180.js";import"./index.89e6767d.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.b5140ae1.js.map
